779303ca5495407f09e53c5b5ddda668ca4ebc91
Kacper Ceszke <kazkwincjusz@outlook.com>
2025-10-17 12:21:44 +0200

    init Configure Django Authentication Backend

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -16,17 +16,9 @@ import sentry_sdk
 # change debug moge to False to check out sentry
 sentry_sdk.init(
     dsn="https://fa3ba76b5c16ea2cd3f62664087d19aa@o4509238287728640.ingest.de.sentry.io/4509238291988560",
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
     traces_sample_rate=1.0,
-    # Set profile_session_sample_rate to 1.0 to profile 100%
-    # of profile sessions.
     profile_session_sample_rate=1.0,
-    # Set profile_lifecycle to "trace" to automatically
-    # run the profiler on when there is an active transaction
     profile_lifecycle="trace",
 )
 
@@ -62,6 +54,7 @@ INSTALLED_APPS = [
     'listalbum',
     'corsheaders',
     'django_filters',
+    'user_app',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +66,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -154,3 +152,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/files/"
+
+
+AUTH_USER_MODEL = 'user_app.CustomUser'
